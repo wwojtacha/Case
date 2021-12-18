c7efe20354944f0f0ff3d14bb496858bb0e974d5
wwojtacha <wwojtacha@o2.pl>
2021-12-18 21:21:19 +0100

    initial commit

---
+++ casemanger/src/main/java/casemanager/CasemangerApplication.java
@@ -0,0 +1,13 @@
+package casemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasemangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasemangerApplication.class, args);
+	}
+
+}
---
+++ casemanger/src/test/java/casemanager/CasemangerApplicationTests.java
@@ -0,0 +1,13 @@
+package casemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasemangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
